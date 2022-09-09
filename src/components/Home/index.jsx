7c5b2315ac00e7cc59f4fe86a781cc856876f618
Zaniuk <61323637+Zaniuk@users.noreply.github.com>
2022-09-09 11:18:38 -0300

    Improved home page

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -7,25 +7,49 @@ export default function Home() {
   return (
     <div>
       <div className="landing">
-        <h1>Hi! <span className="highlight bold">I'm Geronimo Zaniuk</span>, </h1>
-        <h2 className="color-blue bold">A Full-stack Web Developer</h2>
-        <p>
-          Natural <span className="color-blue bold">challenge-seeker</span>, a <span className="color-blue bold">fast-learner</span> and <span className="color-blue bold">multidisciplinary</span> person.
-          I'm searching for my first oportunity to work on a software
-          development team.
-        </p>
+        <motion.h1
+          initial={{ opacity: 0, y: -100 }}
+          animate={{ opacity: 1, y: 0 }}
+        >
+          Hi! <span className="highlight bold">I'm Geronimo Zaniuk</span>,{" "}
+        </motion.h1>
+        <motion.h2
+          initial={{ opacity: 0, y: -100 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ delay: 0.3 }}
+          className="color-blue bold"
+        >
+          A Full-stack Web Developer
+        </motion.h2>
+        <motion.p
+          initial={{ opacity: 0, y: -100 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ delay: 0.6 }}
+        >
+          Natural <span className="color-blue bold">challenge-seeker</span>, a{" "}
+          <span className="color-blue bold">fast-learner</span> and{" "}
+          <span className="color-blue bold">multidisciplinary</span> person. I'm
+          searching for my first oportunity to work on a software development
+          team.
+        </motion.p>
         <Link to="/contact">
-          <motion.button
-            className="contact-button"
-            initial={{backgroundColor: '#0496FF', color: '#fff', scale: 1}}
-            whileHover={{
-              backgroundColor: '#6b5bd4',
-              scale: 1.2,
-              transition: { ease: 'circIn', duration: 0.5 },
-            }}
+          <motion.span
+            initial={{ opacity: 0, y: -100 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ delay: 0.9 }}
           >
-            Contact Me
-          </motion.button>
+            <motion.button
+              className="contact-button"
+              initial={{ backgroundColor: "#0496FF", color: "#fff", scale: 1 }}
+              whileHover={{
+                backgroundColor: "#6b5bd4",
+                scale: 1.2,
+                transition: { type: "spring", stiffness: 300, bounce: 0.5 },
+              }}
+            >
+              Contact Me
+            </motion.button>
+          </motion.span>
         </Link>
       </div>
     </div>
