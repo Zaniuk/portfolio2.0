404430f253a147430879961e8b9bdbccd402cb24
Zaniuk <61323637+Zaniuk@users.noreply.github.com>
2022-09-11 20:44:57 -0300

    fixed non-working TableOfContents component

--- src/components/Blog/TableOfContents.jsx
+++ src/components/Blog/TableOfContents.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import { Viewer } from '@bytemd/react'
 export default function TableOfContents({ lines }) {
     let str = ''
-    const linesMD = lines.map((line, index) => {
-        str += `- [${line.title}](#${line.slug})\n`
+    lines.map((line, index) => {
+        str += `- [${line.title}](#user-content-${line.slug})\n`
     })
     console.log(str)
     return (
