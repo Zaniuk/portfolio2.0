2336bf890797861df7a3838c590edd0296acee8c
Zaniuk <61323637+Zaniuk@users.noreply.github.com>
2022-11-03 09:59:29 -0300

    create certification field

---
+++ src/components/Backoffice/routes/Certifications/Fields.js
@@ -0,0 +1,24 @@
+const certificationFields = [
+    {
+        name: 'name',
+        label: 'Name',
+        type: 'text',
+    },
+    {
+        name: 'description',
+        label: 'Description',
+        type: 'text',
+    },
+    {
+        name: 'company',
+        label: 'Company',
+        type: 'text',
+    },
+    {
+        name: 'image',
+        label: 'Image',
+        type: 'image',
+    }
+]
+
+export default certificationFields
