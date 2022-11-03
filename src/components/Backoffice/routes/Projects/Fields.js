09c75d0d4abc65389cabbc75c5cb6c63c6b98e19
Zaniuk <61323637+Zaniuk@users.noreply.github.com>
2022-11-02 22:51:16 -0300

    create Fields to reuse fields at edit and create projects

---
+++ src/components/Backoffice/routes/Projects/Fields.js
@@ -0,0 +1,34 @@
+const projectFields = [
+    {
+        name: 'title',
+        label: 'Title',
+        type: 'text',
+      },
+      {
+        name: 'github',
+        label: 'Github',
+        type: 'text',
+      },    
+      {
+        name: 'status',
+        label: 'Status',
+        type: 'select',
+        options: [
+          { value: 'active', label: 'Active', default: true },
+          { value: 'finished', label: 'Finished',  default: false },
+        ],
+      },
+      {
+        name: 'description',
+        label: 'Description',
+        type: 'ckeditor',
+      },
+     
+      {
+        name: 'image',
+        label: 'Image',
+        type: 'file',
+      },
+     
+]
+export default projectFields
