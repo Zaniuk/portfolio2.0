0bfbcf3c8c9b886d5940a02b12aec5b40882652a
Zaniuk <61323637+Zaniuk@users.noreply.github.com>
2022-11-02 23:39:08 -0300

    implement delete action callback

--- src/components/Backoffice/routes/Projects/BackofficeProjects.jsx
+++ src/components/Backoffice/routes/Projects/BackofficeProjects.jsx
@@ -2,15 +2,19 @@ import { Box } from '@mui/system'
 import React, { useEffect, useState } from 'react'
 import httpService from '../../../../services/httpService'
 import CustomTable from '../../CustomTable'
-
+import { alert } from '../../../Alert/Alert'
+import { useNavigate } from 'react-router-dom'
+import { Typography } from '@mui/material'
 export default function BackofficeProjects() {
-  
+  const navigate = useNavigate()
+  const [data, setData] = useState([])
   const [items, setItems] = useState([])
   const [headers, setHeaders] = useState([])
   const [actions, setActions] = useState(false)
   const getData = async () => {
     const res = await httpService.get('/projects')
     setHeaders(['Title', 'Description', 'Status'])
+    setData(res.data)
     const items = res.data.map((item) => {
       return {
         rows: [item.title, item.description, item.status],
@@ -23,11 +27,20 @@ export default function BackofficeProjects() {
       edit: true,
       delete: true,
       callbacks: {
+        create: () => navigate('/backoffice/projects/create'),
         edit: async (id) => {
-          console.log(id)
+          navigate(`/backoffice/projects/${id}`)
         },
         delete: async (id) => {
-          console.log(id)
+
+          try{
+            await httpService.delete('/projects/' + id)            
+            alert("Exitoso!", "Se ha borrado el proyecto", "success", false);
+            getData()
+          }catch(e){
+            console.error(e)
+          }
+          
         }
       }
 
@@ -38,6 +51,9 @@ export default function BackofficeProjects() {
   }, [])
   return (
     <Box>
+      <Typography  align='center' variant="h3" color="secondary" sx={{ mb: 2 }}>
+        Projects
+      </Typography>
       <CustomTable items={items} headers={headers} actions={actions} />
     </Box>
   )
