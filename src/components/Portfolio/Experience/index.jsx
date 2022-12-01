de5dcb8ab43dac0e07f1c39cd8bf61ec3581a31e
Zaniuk <61323637+Zaniuk@users.noreply.github.com>
2022-12-01 15:18:35 -0300

    delete unused dependencies

--- src/components/Portfolio/Experience/index.jsx
+++ src/components/Portfolio/Experience/index.jsx
@@ -1,16 +1,5 @@
 import * as React from 'react';
 import Timeline from '@mui/lab/Timeline';
-import TimelineItem from '@mui/lab/TimelineItem';
-import TimelineSeparator from '@mui/lab/TimelineSeparator';
-import TimelineConnector from '@mui/lab/TimelineConnector';
-import TimelineContent from '@mui/lab/TimelineContent';
-import TimelineOppositeContent from '@mui/lab/TimelineOppositeContent';
-import TimelineDot from '@mui/lab/TimelineDot';
-import FastfoodIcon from '@mui/icons-material/Fastfood';
-import LaptopMacIcon from '@mui/icons-material/LaptopMac';
-import HotelIcon from '@mui/icons-material/Hotel';
-import RepeatIcon from '@mui/icons-material/Repeat';
-import Typography from '@mui/material/Typography';
 import ExperienceItem from './ExperienceItem';
 const experience = [
     {
